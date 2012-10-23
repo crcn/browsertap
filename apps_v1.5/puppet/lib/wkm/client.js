92617edfcb1b81285b122dcc9470110a4e0301b2
Craig Condon <craig@spiceapps.com>
2012-10-23 17:05:01 -0500

    fix w/h issues

--- apps_v1.5/puppet/lib/wkm/client.js
+++ apps_v1.5/puppet/lib/wkm/client.js
@@ -59,6 +59,9 @@ module.exports = structr({
 
 		this.kill(function() {
 
+			console.log(video.padding);
+			console.log(video.height)
+
 			video.padding = _.default(video.padding, { left: 0, right: 0, top: 0, bottom: 0 })
 			var args = [
 				"-o", self._rtmpUrl, 
