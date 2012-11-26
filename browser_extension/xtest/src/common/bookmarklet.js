cf9141e5f45ff3e291620951b891567fbe2e5641
Craig Condon <craig@spiceapps.com>
2012-11-26 11:51:22 -0600

    cleanup

--- browser_extension/xtest/src/common/bookmarklet.js
+++
@@ -1,6 +0,0 @@
-(function() {
-	var script = document.createElement("script");
-	script.src = "http://maestro.browsertap.com:8080/js/extension.js"
-	script.type = "text/javascript";
-	document.getElementsByTagName("head")[0].appendChild(script);
-})();
