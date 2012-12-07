e0f622ad7164717eb6d8446d4afd4afe3610db8b
Craig Condon <craig@spiceapps.com>
2012-12-06 18:01:25 -0600

    cneaup

--- application/lib/front-plugins/app.http.client/public/app/plugins/init/index.js
+++ application/lib/front-plugins/app.http.client/public/app/plugins/init/index.js
@@ -67,7 +67,7 @@ exports.plugin = function(router, bark, mainPlugin, puppeteer, commands, loader)
 		defaults = { qmin: 1, qmax: 5, gop_size: 70, frame_rate: 40 };
 
 
-		console.log(q)
+		console.log(JSON.stringify(q))
 		for(var key in defaults) {
 			if(q[key]) {
 				q[key] = Number(q[key]);
@@ -76,6 +76,7 @@ exports.plugin = function(router, bark, mainPlugin, puppeteer, commands, loader)
 			}
 		}
 
+		console.log(q)
 
 		win.startRecording(q, function(err, info) {
 
