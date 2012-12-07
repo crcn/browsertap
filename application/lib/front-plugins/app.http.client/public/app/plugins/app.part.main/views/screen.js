1b2fb620ad28f8fdd9078eb527b1191e3ebaab50
Craig Condon <craig@spiceapps.com>
2012-12-06 18:13:01 -0600

    cleanup

--- application/lib/front-plugins/app.http.client/public/app/plugins/app.part.main/views/screen.js
+++ application/lib/front-plugins/app.http.client/public/app/plugins/app.part.main/views/screen.js
@@ -44,7 +44,7 @@ module.exports = require("../../../views/base").extend({
 		});
 
 		this._disposable.addInterval(setInterval(_.bind(this.syncScrollInfo, this), 2000));
-		// this._disposable.addInterval(setInterval(_.bind(this._changeVideoQuality, this), 200));
+		this._disposable.addInterval(setInterval(_.bind(this._changeVideoQuality, this), 200));
 		this._window.bindProxy(_.bind(this.onProxy, this));
 
 		this.onResize();
@@ -135,6 +135,7 @@ module.exports = require("../../../views/base").extend({
 	"onMouseMove": function(e) {
 
 
+		this._prevMousePosition = Math
 
 		this._lastMouseMoveAt = Date.now();
 
@@ -147,10 +148,6 @@ module.exports = require("../../../views/base").extend({
 
 		if(this.coords) {
 			this._mouseMoveDelta = Math.round(Math.sqrt(Math.pow(this.coords.x - coords.x, 2) + Math.pow(this.coords.y - coords.y, 2)))
-		}
-
-		if(this._mouseDown) {
-			this._changeVideoQuality();
 		}
 
 		this._window.mouseEvent(wkmEvents.mouse.MOUSEEVENTF_ABSOLUTE | wkmEvents.mouse.MOUSEEVENTF_MOVE, this.coords = coords);
@@ -194,7 +191,7 @@ module.exports = require("../../../views/base").extend({
 		}
 
 
-		this._changeVideoQuality();
+		// this._changeVideoQuality();
 	},
 	"syncScrollInfo": function() {
 		if(!this.proxy) return;
