3427ab3778d2d7afed5c7348467abd44d31c60cf
Craig Condon <craig@spiceapps.com>
2013-01-04 01:09:11 -0600

    cleanup preloader

--- application/lib/front-plugins/app.http.client/public/app/plugins/app.part.main/views/loader.js
+++ application/lib/front-plugins/app.http.client/public/app/plugins/app.part.main/views/loader.js
@@ -3,9 +3,15 @@ module.exports = require("../../../views/base").extend({
 	"initialize": function() {
 		module.exports.__super__.initialize.call(this);
 		
-		var self = this;
+		var self = this, cp = 0;
 
 		function onState(state) {
+
+			if(state.p < cp) {
+				$(self.el).find("#progress").css({width:0});
+			}
+			cp = state.p;
+
 			$(self.el).find("#progress").transit({ width: (state.p * 100) + "%"});
 		}
 
@@ -14,16 +20,23 @@ module.exports = require("../../../views/base").extend({
 	"showNotification": function() {
 		if(this._showing) return;
 		this._showing = true;
-		$(self.el).find("#progress").css({ width: "0%"});
+		$(self.el).find("#progress").css({ width: "200px" });
 		$(this.el).css({opacity:0, scale:0.75, display:"block"});
 		$(this.el).transit({opacity:0.85, scale:1 }, 500, "ease");
 		return this;
 	},
 	"update": function(data) {
 		if(!data) data = {};
+		var img = data.app ? "/img/apps/"+data.app.toLowerCase()+".png" : null, self = this,
+		$ic = $(self.el).find("#icon-inner-container").css({ "visibility": "hidden" });
 
-		$(this.el).find("img").attr("src", data.app ? "/img/apps/"+data.app.toLowerCase()+".png" : null);
-		$(this.el).find("#progress-text").text(data.app.substr(0, 1).toUpperCase() + data.app.substr(1) +" "+data.version)
+
+		$(this.el).find("img").attr("src", img).one("load", function() {
+			$ic.css({ "visibility": "visible" }).width($(this).width()).height($(this).height());
+		})
+		$(this.el).find("#progress").css({ "background-image": "url("+img+")"});
+		$(this.el).find("#progress-text").text(data.app.substr(0, 1).toUpperCase() + data.app.substr(1) +" "+data.version);
+
 
 	},
 	"hideNotification": function(callback) {
