d1c1d8d6d6f391c7afb687185d98bd0681c256b6
Craig Condon <craig@spiceapps.com>
2013-01-08 15:54:11 -0600

    change framerate to 24

--- application/lib/front-plugins/app.http.client/public/app/plugins/init/index.js
+++ application/lib/front-plugins/app.http.client/public/app/plugins/init/index.js
@@ -91,7 +91,7 @@ exports.plugin = function(router, bark, states, mainPlugin, puppeteer, commands,
 		var q = Url.parse(String(window.location), true).query,
 
 		//low GOP initially so there's no delay when interacting with the page
-		defaults = { qmin: 1, qmax: 5, gop_size: 70, frame_rate: 18 };
+		defaults = { qmin: 1, qmax: 5, gop_size: 70, frame_rate: 24 };
 
 
 		for(var key in defaults) {
