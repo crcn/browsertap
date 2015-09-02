ce2e427df946279d231637ad3cd3f480fa40f3ff
Craig Condon <craig.j.condon@gmail.com>
2015-09-01 22:52:08 -0700

    use es6 classes

--- apps/common/schemas/schema.js
+++ apps/common/schemas/schema.js
@@ -1,36 +1,40 @@
-var extend = require("lodash/object/extend");
-var sift   = require("sift");
-var get    = require("common/utils/get-property");
-
-/**
- */
-function PropertyValidator(property, options) {
-
-  this.property = property;
-
-  extend(this, options);
-
-  this.required = options.required;
-  var validate = options.validate;
-
-  if (typeof validate === "object") {
-    validate = sift(validate);
-  }
-
-  if (!validate) {
-    validate = function() {
-      return true;
-    };
-  }
-
-  this.validate = validate;
-}
+import extend from "lodash/object/extend";
+import sift from "sift";
+import get from "common/utils/get-property";
 
 /**
  */
 
-extend(PropertyValidator.prototype, {
-  getError: function(value) {
+class PropertyValidator {
+
+  /**
+   */
+
+  constructor(property, options) {
+    this.property = property;
+
+    extend(this, options);
+
+    this.required = options.required;
+    var validate = options.validate;
+
+    if (typeof validate === "object") {
+      validate = sift(validate);
+    }
+
+    if (!validate) {
+      validate = function() {
+        return true;
+      };
+    }
+
+    this.validate = validate;
+  }
+
+  /**
+   */
+
+  getError(value) {
 
     if (value == void 0) {
       if (this.required) return new Error("not defined");
@@ -40,32 +44,33 @@ extend(PropertyValidator.prototype, {
     if (!this.validate(value)) {
       return new Error("invalid");
     }
-  },
-  serialize: function(value) { return value; }
-});
-
-/**
- */
-
-function Schema(properties) {
-  if (!properties) properties = {};
-
-  this.__validators = [];
-
-  for (var key in properties) {
-    this.__validators.push(new PropertyValidator(key, properties[key]));
   }
-}
-
-/**
- */
-
-extend(Schema.prototype, {
 
   /**
    */
 
-  getError: function(propsFilter, data) {
+  serialize(value) { return value; }
+};
+
+class Schema {
+
+  /**
+   */
+
+  constructor(properties) {
+    if (!properties) properties = {};
+
+    this.__validators = [];
+
+    for (var key in properties) {
+      this.__validators.push(new PropertyValidator(key, properties[key]));
+    }
+  }
+
+  /**
+   */
+
+  getError(propsFilter, data) {
 
     if (arguments.length === 1) {
       data        = propsFilter;
@@ -80,12 +85,12 @@ extend(Schema.prototype, {
       if ((error = validator.getError(get(data, validator.property)))) return error;
     }
     return error;
-  },
+  }
 
   /**
    */
 
-  serialize: function(data) {
+  serialize(data) {
     var validators = this.getValidators();
     var newData = {};
     for (var i = validators.length; i--;) {
@@ -95,20 +100,20 @@ extend(Schema.prototype, {
       newData[validator.property] = validator.serialize(value);
     }
     return newData;
-  },
+  }
 
   /**
    */
 
-  validate: function() {
+  validate() {
     return !this.getError.apply(this, arguments);
-  },
+  }
 
   /**
    * pluck properties
    */
 
-  pluck: function(propsFilter, data) {
+  pluck(propsFilter, data) {
     var keys = this.getValidators(propsFilter).map(function(validator) {
       return validator.property;
     });
@@ -119,12 +124,12 @@ extend(Schema.prototype, {
       if (value != void 0) newData[key] = value;
     }
     return newData;
-  },
+  }
 
   /**
    */
 
-  getValidators: function(filter) {
+  getValidators(filter) {
     if (filter) {
       if (typeof filter !== "function") {
         filter = sift(filter);
@@ -134,7 +139,8 @@ extend(Schema.prototype, {
       return this.__validators;
     }
   }
-});
+}
+
 
 /**
  */
