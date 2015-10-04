2a6ecf9597eea554b1a533b4b41af32a7a736ec8
Craig Condon <craig.j.condon@gmail.com>
2015-10-03 18:39:43 -0700

    update gulp task

--- apps/desktop/gulpfile.js
+++ apps/desktop/gulpfile.js
@@ -4,17 +4,19 @@ var spawn         = child_process.spawn;
 var co            = require("co");
 var mergeStreams  = require("merge-stream");
 var rmdir         = _promisify(require("rmdir"));
-
+var trim          = require("lodash/string/trim");
 
 var paths = {
 
 };
 
 _task("default", make);
+_task("link", link);
 _task("run", run);
 _task("clean", clean);
 _task("prepare", prepare);
 _task("make", make);
+_task("test", test);
 
 function* clean() {
   yield rmdir(__dirname + "/build");
@@ -26,13 +28,28 @@ function* run() {
   });
 }
 
+function* link() {
+  yield _spawn("mkdir", ["-p", "/usr/local/bt"]);
+  yield _spawn("rm", ["-rf", "/usr/local/bt/remote-desktop-server"]);
+  yield _spawn("ln", ["-s", trim(yield _spawn("pwd")), "/usr/local/bt/remote-desktop-server"]);
+}
+
 function* prepare() {
+  yield link();
   yield _spawn("vendor/gyp/gyp", ["remote_desktop_server.gyp", "--depth=.", "-f", "make", "--generator-output=./build/app"]);
+  yield _spawn("vendor/gyp/gyp", ["tests.gyp", "--depth=.", "-f", "make", "--generator-output=./build/app_test"]);
 }
 
 function* make() {
   yield prepare();
   yield _spawn("make", ["-C", "./build/app", "V=1"]);
+  yield _spawn("make", ["-C", "./build/app_test", "V=1"]);
+}
+
+function* test() {
+  yield _spawn("./app_test", [], {
+    cwd: __dirname + "/build/app_test/out/Release"
+  });
 }
 
 function _task(name, args, callback) {
@@ -50,16 +67,21 @@ function _task(name, args, callback) {
 function _spawn(command, args, ops) {
   return new Promise(function(resolve, reject) {
 
+    console.log(command, (args || []).join(" "));
+
     var proc = spawn(command, args, Object.assign({
       cwd: __dirname
     }, ops || {}));
 
+    var buffer = [];
+
+    proc.stdout.on("data", buffer.push.bind(buffer));
     proc.stdout.pipe(process.stdout);
     proc.stderr.pipe(process.stderr);
 
     proc.on("close", function(err, code) {
       if (err) return reject(err);
-      resolve();
+      resolve(buffer.join(""));
     });
   });
 }
@@ -68,7 +90,6 @@ function _promisify(method) {
   return function() {
     var args = Array.prototype.slice.call(arguments);
     return new Promise(function(resolve, reject) {
-      console.log(args);
       method.apply(global, args.concat(function(err, result) {
         if (err) return reject(err);
         resolve(result);
